30e1a6ff08ac3a1ffa8a399f50cb4fde29821820
BLUEHORSE 123 <siddhartha.ee.13@gmail.com>
2016-09-06 20:31:20 +0530

    Initial commit

---
+++ app/src/main/java/in/creativelizard/recorditsound/MainActivity.java
@@ -0,0 +1,24 @@
+package in.creativelizard.recorditsound;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initialize();
+    }
+
+    private void initialize() {
+
+    }
+
+    public void clkRecord(View v){
+        
+    }
+
+}
